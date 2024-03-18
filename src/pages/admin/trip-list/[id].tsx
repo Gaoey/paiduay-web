be7727f038d2c259267976d69af312cc125b9e6d
gaoey <>
2024-03-18 13:22:21 +0700

    feat: update trip details

--- src/pages/admin/trip-list/[id].tsx
+++ src/pages/admin/trip-list/[id].tsx
@@ -1,16 +1,27 @@
+import { Grid } from '@mui/material'
 import { getSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { ReactNode } from 'react'
+import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
+import AdminLayout from 'src/layouts/AdminLayout'
+import TripDetailComponent from 'src/views/admin/TripDetail'
 
 export default function TripDetail() {
   const router = useRouter()
 
   return (
-    <div>
-      <p>Post: {router.query.id}</p>
-    </div>
+    <ApexChartWrapper>
+      <Grid container spacing={7}>
+        <Grid item xs={12} md={6}>
+          <TripDetailComponent tripID={router.query.id as string} />
+        </Grid>
+        <Grid item xs={12} md={6}></Grid>
+      </Grid>
+    </ApexChartWrapper>
   )
 }
+
+TripDetail.getLayout = (page: ReactNode) => <AdminLayout>{page}</AdminLayout>
 
 export async function getServerSideProps(ctx: any) {
   const session = await getSession(ctx)
